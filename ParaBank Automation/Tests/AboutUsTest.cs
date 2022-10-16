8f3e54638220aa9e175cbacc017de426df236490
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-10-16 23:25:27 +0300

    Added LoginPanel  and LoginPanelTests

---
+++ ParaBank Automation/Tests/AboutUsTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using ParaBank_Automation.Pages;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaBank_Automation.Tests
+{
+    internal class AboutUsTest : BaseTest
+    {
+        
+        [Test]
+        public void OpenAboutUsPage()
+        {
+            var page = new AboutUs(driver);
+            page.Open();
+            Assert.AreEqual("ParaBank | About Us", page.GetPageTitle());
+            Assert.AreEqual("ParaSoft Demo Website", page.GetPageHeadingText());
+
+        }
+    }
+}
