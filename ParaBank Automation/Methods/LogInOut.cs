d9a6733a6660ecb2c0e58358234966472721447e
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-10-23 21:48:00 +0300

    Add method folder
    - class contains methods for login user and logout user

---
+++ ParaBank Automation/Methods/LogInOut.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using ParaBank_Automation.Utilities;
+
+namespace ParaBank_Automation.Methods
+{
+    
+    public  static class LogInOut
+    {
+        public static void LogInUser(IWebElement usernameField,IWebElement passowrdField,IWebElement button)
+        {
+           if (usernameField.Displayed && passowrdField.Displayed && button.Displayed)
+            {
+                usernameField.SendKeys(Constants.username);
+                passowrdField.SendKeys(Constants.password);
+                button.Click();
+            }
+        }
+        public static void LogOutUser(IWebElement logOutButton)
+        {
+            if(logOutButton.Displayed)
+            {
+                logOutButton.Click();
+            }
+        }
+       
+    }
+}
